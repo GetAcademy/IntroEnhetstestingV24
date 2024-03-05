ee5889c07228fd5e0afb0ba508ba2ebb2a42ff5f
GET Academy <terje@kolderup.net>
2024-03-05 10:47:24 +0100

    x

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function translateText(text){
-    return text.length;
+function translateText(text) {
+    if (!text || typeof(text) != 'string' ) return '';
+    let translatedText = '';
+    for (let character of text) {
+        translatedText += character;
+        const lowChar = character.toLowerCase();
+        const isConsonant = 'bcdfghjklmnpqrstvwxz'.includes(lowChar);
+        if (isConsonant) {
+            translatedText += 'o' + lowChar;
+        }
+    }
+    return translatedText;
 }
